d1ad3ee198dec8d040e2d04a838e5db0d02f28d1
Takahiro KITAHARA <fliedonion@gmail.com>
2020-05-30 23:46:54 +0900

    add sample code

---
+++ ImeModeWithSetInputScopeApi/SampleForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace case_of_t.ImeModeWithSetInputScopeApi {
+    partial class SampleForm {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.KatakanaHalf;
+            this.textBox2.Location = new System.Drawing.Point(209, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(209, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 19);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ImeMode.KatakanaHalf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SetInputScope.KatakanaHalf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "SetInputScope";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImeModeWithSetInputScopeApi/SampleForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace case_of_t.ImeModeWithSetInputScopeApi {
+    public partial class SampleForm : Form {
+        // Sample for against IME mode model per-user 
+
+        // note of SetInputScope / SetInputScope2:
+        // The application must call SetInputScope with IS_DEFAULT before a window is destroyed to clear the reference of the interface.
+
+        public SampleForm() {
+            InitializeComponent();
+
+            Load += SampleForm_Load;
+            FormClosing += SampleForm_FormClosing;
+        }
+
+        void SampleForm_Load(object sender, EventArgs e) {
+             SetInputScope(textBox3.Handle, (int)itf_inputscopes.IS_KATAKANA_HALFWIDTH);
+        }
+
+        void SampleForm_FormClosing(object sender, FormClosingEventArgs e) {
+            SetInputScope(textBox3.Handle, (int)itf_inputscopes.IS_DEFAULT);
+        }
+
+
+        [DllImport("Msctf.dll", PreserveSig = false)]
+        private static extern void SetInputScope(IntPtr handle, int inputScope);
+
+
+        enum itf_inputscopes : int
+        {	
+            IS_DEFAULT	= 0,
+	        IS_URL	= 1,
+	        IS_FILE_FULLFILEPATH	= 2,
+	        IS_FILE_FILENAME	= 3,
+	        IS_EMAIL_USERNAME	= 4,
+	        IS_EMAIL_SMTPEMAILADDRESS	= 5,
+	        IS_LOGINNAME	= 6,
+	        IS_PERSONALNAME_FULLNAME	= 7,
+	        IS_PERSONALNAME_PREFIX	= 8,
+	        IS_PERSONALNAME_GIVENNAME	= 9,
+	        IS_PERSONALNAME_MIDDLENAME	= 10,
+	        IS_PERSONALNAME_SURNAME	= 11,
+	        IS_PERSONALNAME_SUFFIX	= 12,
+	        IS_ADDRESS_FULLPOSTALADDRESS	= 13,
+	        IS_ADDRESS_POSTALCODE	= 14,
+	        IS_ADDRESS_STREET	= 15,
+	        IS_ADDRESS_STATEORPROVINCE	= 16,
+	        IS_ADDRESS_CITY	= 17,
+	        IS_ADDRESS_COUNTRYNAME	= 18,
+	        IS_ADDRESS_COUNTRYSHORTNAME	= 19,
+	        IS_CURRENCY_AMOUNTANDSYMBOL	= 20,
+	        IS_CURRENCY_AMOUNT	= 21,
+	        IS_DATE_FULLDATE	= 22,
+	        IS_DATE_MONTH	= 23,
+	        IS_DATE_DAY	= 24,
+	        IS_DATE_YEAR	= 25,
+	        IS_DATE_MONTHNAME	= 26,
+	        IS_DATE_DAYNAME	= 27,
+	        IS_DIGITS	= 28,
+	        IS_NUMBER	= 29,
+	        IS_ONECHAR	= 30,
+	        IS_PASSWORD	= 31,
+	        IS_TELEPHONE_FULLTELEPHONENUMBER	= 32,
+	        IS_TELEPHONE_COUNTRYCODE	= 33,
+	        IS_TELEPHONE_AREACODE	= 34,
+	        IS_TELEPHONE_LOCALNUMBER	= 35,
+	        IS_TIME_FULLTIME	= 36,
+	        IS_TIME_HOUR	= 37,
+	        IS_TIME_MINORSEC	= 38,
+	        IS_NUMBER_FULLWIDTH	= 39,
+	        IS_ALPHANUMERIC_HALFWIDTH	= 40,
+	        IS_ALPHANUMERIC_FULLWIDTH	= 41,
+	        IS_CURRENCY_CHINESE	= 42,
+	        IS_BOPOMOFO	= 43,
+	        IS_HIRAGANA	= 44,
+	        IS_KATAKANA_HALFWIDTH	= 45,
+	        IS_KATAKANA_FULLWIDTH	= 46,
+	        IS_HANJA	= 47,
+	        IS_HANGUL_HALFWIDTH	= 48,
+	        IS_HANGUL_FULLWIDTH	= 49,
+	        IS_PHRASELIST	= -1,
+	        IS_REGULAREXPRESSION	= -2,
+	        IS_SRGS	= -3,
+	        IS_XML	= -4,
+	        IS_ENUMSTRING	= -5
+        }
+
+    }
+}
